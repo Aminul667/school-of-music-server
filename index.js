61b024f7354212f6a3b7bb450db4d9cc9cfa3ab9
Aminul667 <rahathssn@gmail.com>
2023-06-10 23:45:28 +0600

    added classes collections

--- index.js
+++ index.js
@@ -10,31 +10,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// JWT verification 
+// JWT verification
 const verifyJWT = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
       return res
         .status(401)
         .send({ error: true, message: "unauthorized access" });
     }
-    // bearer token
-    const token = authorization.split(" ")[1];
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res
-          .status(401)
-          .send({ error: true, message: "unauthorized access" });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  };
+    req.decoded = decoded;
+    next();
+  });
+};
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// mongodb connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbyf5is.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,16 +42,51 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const usersCollection = client.db("musicUser").collection("users");
+    const classCollection = client.db("musicUser").collection("classes");
+
+    // create user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already existed" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    // class related API
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+    // end of the database
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -62,9 +96,9 @@ run().catch(console.dir);
 
 // running server
 app.get("/", (req, res) => {
-    res.send("SchoolOfMusic is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`SchoolOfMusic is running on the port ${port}`);
-  });
+  res.send("SchoolOfMusic is running");
+});
+
+app.listen(port, () => {
+  console.log(`SchoolOfMusic is running on the port ${port}`);
+});
