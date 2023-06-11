7c3b6171315307464f13f2d2da10efc20e954311
Aminul667 <rahathssn@gmail.com>
2023-06-11 14:20:19 +0600

    added role

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const verifyJWT = (req, res, next) => {
 };
 
 // mongodb connection
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbyf5is.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -65,6 +65,24 @@ async function run() {
       res.send(result);
     });
 
+    // verifyJWT, verifyAdmin,
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // class related API
     app.get("/classes", async (req, res) => {
